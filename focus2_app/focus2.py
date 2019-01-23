357f81a3bf57cc573e0521e120ae7c8d2f4a4d47
metageni <geni.silva@helix.com>
2019-01-22 21:42:48 -0800

    basic structure focus2

---
+++ focus2_app/focus2.py
@@ -0,0 +1,124 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import argparse
+import csv
+import logging
+import os
+import random
+
+from pathlib import Path
+from collections import defaultdict
+
+from focus2_app import version
+
+LOGGER_FORMAT = '[%(asctime)s - %(levelname)s] %(message)s'
+logging.basicConfig(format=LOGGER_FORMAT, level=logging.INFO)
+LOGGER = logging.getLogger(__name__)
+
+
+def which(program_name):
+    """python implementation of unix 'which' function.
+
+    Args:
+        program_name (str): Program name
+
+    Returns:
+        None or str: Program path
+
+    """
+
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program_name)
+    if fpath:
+        if is_exe(program_name):
+            return program_name
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            path = path.strip('"')
+            exe_file = os.path.join(path, program_name)
+            if is_exe(exe_file):
+                return exe_file
+
+
+def is_wanted_file(queries):
+    """Remove files from query files that not have extension .fasta/.fastq/.fna
+
+    Args:
+        queries (list): List with query names
+
+    Returns:
+        list: sorted list with only .fasta/.fastq/.fna files
+
+    """
+    queries = [query for query in queries if query.split(".")[-1].lower() in ["fna", "fasta", "fastq"]]
+    queries.sort()
+
+    return queries
+
+
+def parse_args():
+    """Parse args entered by the user.
+
+    Returns:
+        argparse.Namespace: parsed arguments
+
+    """
+    parser = argparse.ArgumentParser(
+        description='FOCUS2: Agile and sensitive classification of metagenomics data using '
+                    'a reduced database', epilog='example > focus2 -q INPUT_DIR -o OUTPUT')
+    parser.add_argument('-v', '--version', action='version', version='FOCUS {}'.format(version))
+    parser.add_argument('-q', '--query', help='Path to directory with FAST(A/Q) files', required=True)
+    parser.add_argument('-o', '--output_directory', help='Path to output files', required=True)
+    parser.add_argument('-p', '--output_prefix', help='Output prefix (Default: output)', default='output')
+    parser.add_argument("-b", "--alternate_directory", help="Alternate directory for your databases", default="")
+    parser.add_argument('-t', '--threads', help='Number Threads used during alignment (Default: 4)', default='4')
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+
+    # parameters and other variables
+    query = Path(args.query)
+    prefix = args.output_prefix
+    output_directory = Path(args.output_directory)
+    WORK_DIRECTORY = Path(args.alternate_directory) if args.alternate_directory else Path(__file__).parents[0]
+    threads = args.threads
+    aligner_path = which("bwa")
+
+    LOGGER.info("FOCUS2: Agile and sensitive classification of metagenomics data using a reduced database")
+
+    # check if output_directory is exists - if not, creates it
+    if not output_directory.exists():
+        Path(output_directory).mkdir(parents=True, mode=511)
+        LOGGER.info("OUTPUT: {} does not exist - just created it :)".format(output_directory))
+
+    # check if at least one of the queries is valid
+    if not query.is_dir():
+        LOGGER.critical("QUERY: {} is not a directory".format(query))
+
+    # check if at least one of the queries is valid
+    if is_wanted_file(os.listdir(query)) == []:
+        LOGGER.critical("QUERY: {} does not have any FASTA/FNA/FASTQ file".format(query))
+
+    # check if query is exists
+    elif not query.exists():
+        LOGGER.critical("QUERY: {} does not exist".format(query))
+
+    # check if work directory exists
+    elif WORK_DIRECTORY != WORK_DIRECTORY or not WORK_DIRECTORY.exists():
+        LOGGER.critical("WORK_DIRECTORY: {} does not exist".format(WORK_DIRECTORY))
+
+
+    else:
+        LOGGER.info("1) RUNNING FOCUS2")
+
+    LOGGER.info('Done'.format(output_directory))
+
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+requirements = [
+    'numpy >= 1.12.1',
+    'scipy >= 0.19.0',
+]
+
+test_requirements = [
+    'pytest'
+]
+
+setup_requirements = []
+
+setup(
+    name='metagenomics-focus2',
+    use_scm_version=True,
+    description='FOCUS2: Agile and sensitive classification of metagenomics data using a reduced database',
+    author='Genivaldo G. Z. Silva',
+    author_email='genivaldo.gueiros@gmail.com',
+    url='https://github.com/metageni/focus2',
+    packages=[
+        'focus2_app',
+    ],
+    package_dir={'focus2_app': 'focus2_app'},
+    include_package_data=True,
+    install_requires=requirements,
+    setup_requires=setup_requirements,
+    zip_safe=False,
+    keywords='focus2_app',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+    entry_points={
+        'console_scripts': [
+            'focus2 = focus2_app.focus2:main',
+        ]
+    },
+)
